4c84e13a76d9a3051227fa56e3e13699cad8fb53
Mr <5997507+minro@users.noreply.github.com>
2023-02-20 20:28:46 +0100

    chore(setup): :wrench: renames astro.config to be of type .ts
    To enable writing typeScript in the config file

---
+++ astro.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'astro/config';
+
+// Example: Bare minimum, empty configuration file
+// export default {}
+
+// https://astro.build/config
+export default defineConfig({
+	// your configuration goes here
+	// https://docs.astro.build/en/reference/configuration-reference/
+});
