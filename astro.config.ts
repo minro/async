8ee11995646623413020e9b782df150a6882b6dd
Mr <5997507+minro@users.noreply.github.com>
2023-02-20 23:31:24 +0100

    chore(setup): :wrench: add support for vanilla extract vite plugin

--- astro.config.ts
+++ astro.config.ts
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 
 // Example: Bare minimum, empty configuration file
 // export default {}
@@ -7,4 +9,8 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
 	// your configuration goes here
 	// https://docs.astro.build/en/reference/configuration-reference/
+	integrations: [react()],
+	vite: {
+		plugins: [vanillaExtractPlugin()],
+	}
 });
